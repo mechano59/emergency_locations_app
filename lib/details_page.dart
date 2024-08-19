717284e9be90ed282b6b716fae25d4dc53774fe2
Foyez Ahammed Sharif Plaban <5plaban9@gmail.com>
2024-08-19 18:49:43 +0600

    changed some variable names and syntax of how they are used and optimized the code

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -3,7 +3,7 @@ import 'models/location.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
 class DetailsPage extends StatelessWidget {
-  final Location location;
+  final Map location;
 
   DetailsPage({required this.location});
 
@@ -11,35 +11,55 @@ class DetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(location.title),
+        title: Text(location['title']),
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CachedNetworkImage(
-              imageUrl: 'https://backend.houston24hourer.com/storage/${location.img}',
-              placeholder: (context, url) => CircularProgressIndicator(),
-              errorWidget: (context, url, error) => Icon(Icons.error),
-              width: double.infinity,
-              height: 200,
-              fit: BoxFit.cover,
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              location.title,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8.0),
-            Text('${location.city}, ${location.state}\n${location.address}'),
-            SizedBox(height: 16.0),
-            Text('Phone: ${location.tel}', style: TextStyle(fontSize: 16)),
-            SizedBox(height: 16.0),
-            Text(location.content, style: TextStyle(fontSize: 16)),
-            SizedBox(height: 16.0),
-            Text('Coordinates: (${location.latitude}, ${location.longitude})', style: TextStyle(fontSize: 16)),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                'https://backend.houston24hourer.com/storage/${location['img']}',
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 16),
+              Text(
+                location['title'],
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                '${location['city']}, ${location['state']}',
+                style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+              ),
+              SizedBox(height: 8),
+              Text(
+                location['address'],
+                style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Phone: ${location['tel']}',
+                style: TextStyle(fontSize: 16, color: Colors.blueAccent),
+              ),
+              SizedBox(height: 16),
+              Text(
+                location['content'],
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Latitude: ${location['latitude']}',
+                style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+              ),
+              Text(
+                'Longitude: ${location['longitude']}',
+                style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+              ),
+            ],
+          ),
         ),
       ),
     );
